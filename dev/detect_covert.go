90aa9217bd776391dcf336a4fdb4e834730af198
user <zppratt@gmail.com>
2015-09-06 15:32:07 -0400

    start of dev/detect_covert.go authoring.

---
+++ dev/detect_covert.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// Call tshark and create the .useful file. Returns 0 on success, 1 on error.
+func extract_useful() int {
+	
+}
+
+func organize_ipd(x int, y int) int {
+	return x + y
+}
+
+func extract_useful(x int, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println(add(42, 13))
+}
